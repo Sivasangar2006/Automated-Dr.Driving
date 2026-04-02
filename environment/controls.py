83661fb55d1ce7097f32553248abf37c8a324581
sivasangar2006 <sivasangar2786@gmail.com>
2026-04-02 21:14:50 +0530

    modified reward 1

--- environment/controls.py
+++ environment/controls.py
@@ -6,34 +6,60 @@ pyautogui.FAILSAFE = False
 
 RETRY_BUTTON = (1752, 974)
 
-pyautogui.keyUp("left")
-pyautogui.keyUp("right")
 
-if action == 0:
+def _focus_bluestacks():
+    """
+    Brings BlueStacks window to front so key presses go to the right place.
+    """
+    windows = gw.getWindowsWithTitle("BlueStacks")
+    if windows:
+        try:
+            windows[0].activate()
+        except:
+            pass
+
+
+def perform_action(action: int):
+    """
+    0 = straight
+    1 = left
+    2 = right
+    """
+    _focus_bluestacks()
+
+    # always hold acceleration
     pyautogui.keyDown("up")
     pyautogui.keyUp("down")
-elif action == 1:
-    pyautogui.keyUp("up")
-    pyautogui.keyDown("down")
-elif action == 2:
+
+    # reset steering every step
+    pyautogui.keyUp("left")
+    pyautogui.keyUp("right")
+
+    # apply steering
+    if action == 1:
+        pyautogui.keyDown("left")
+    elif action == 2:
+        pyautogui.keyDown("right")
+    # action 0 = straight, no steering needed
+
+
+def release_all():
     pyautogui.keyUp("up")
     pyautogui.keyUp("down")
+    pyautogui.keyUp("left")
+    pyautogui.keyUp("right")
 
-pyautogui.keyUp("up")
-pyautogui.keyUp("down")
-pyautogui.keyUp("left")
-pyautogui.keyUp("right")
 
-time.sleep(2)
+def restart_mission():
+    """
+    Brings BlueStacks to front and clicks retry button.
+    """
+    time.sleep(2)
 
-windows = gw.getWindowsWithTitle("BlueStacks")
-
-if len(windows) > 0:
-    win = windows[0]
-    win.activate()
+    _focus_bluestacks()
     time.sleep(1)
 
-pyautogui.moveTo(RETRY_BUTTON[0], RETRY_BUTTON[1], duration=0.3)
-pyautogui.click()
+    pyautogui.moveTo(RETRY_BUTTON[0], RETRY_BUTTON[1], duration=0.3)
+    pyautogui.click()
 
-time.sleep(4)
+    time.sleep(4)
